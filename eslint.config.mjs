84c777089b0f39c20bf7fc8b552554b1fce814b2
Selina <selinakimdev@outlook.com>
2026-01-26 14:38:02 -0500

    chore: configure eslint, prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  eslintConfigPrettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -13,6 +16,14 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    plugins: {
+      react: reactPlugin,
+    },
+    rules: {
+      "react/jsx-sort-props": "error",
+    },
+  },
 ]);
 
 export default eslintConfig;
