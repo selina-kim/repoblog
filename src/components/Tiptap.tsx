e4500af3530df73dbf699dcf988b612fd3c32bcb
Selina <selinakimdev@outlook.com>
2026-02-12 00:35:08 -0500

    feat: add syntax highlighting for tiptap editor

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -3,11 +3,20 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { CSSProperties } from "react";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import { all, createLowlight } from "lowlight";
 import "@/styles/mdx.css";
 
 const Tiptap = ({ style }: { style: CSSProperties }) => {
+  const lowlight = createLowlight(all);
+
   const editor = useEditor({
-    extensions: [StarterKit.configure()],
+    extensions: [
+      StarterKit.configure(),
+      CodeBlockLowlight.configure({
+        lowlight,
+      }),
+    ],
     content: "",
     // Don't render immediately on the server to avoid SSR issues
     immediatelyRender: false,
